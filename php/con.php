8e5ace59089a0c9354e80dcf18b62ffb262301ca
nam <39021310+mallarinoel@users.noreply.github.com>
2021-09-07 17:32:44 +0800

    Update con.php

--- php/con.php
+++ php/con.php
@@ -1,9 +1,9 @@
 <?php
 /*I used AWebServer Android App*/
 //Github Deployment -> Date : September/06/2021//
-$host = "0.0.0.0:3306";
-$username = "root";
-$password = "root";
-$dbname = "mydb";
+$host = "sql6.freesqldatabase.com";
+$username = "sql6435254";
+$password = "hAmcla3xqx";
+$dbname = "sql6435254";
 $con = mysqli_connect($host, $username, $password, $dbname);
 ?>
